1a11c8658a5ea35b9f28170b85c8016bce659983
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-13 11:17:20 -0700

    example 2

--- OptionalClass/Rev.java
+++ OptionalClass/Rev.java
@@ -5,10 +5,11 @@ public class Rev {
         String[] str = new String[5];
         str[2] = "Revature classes are coming soon";
 
-        Optional<String> empty = Optional.empty();
-        System.out.println(empty);
-
         Optional<String> value = Optional.of(str[2]);
-        System.out.println(value);
+        System.out.println(value.get());
+
+        System.out.println(value.hashCode());
+
+        System.out.println(value.isPresent());
     }
 }
