fe47fda14108553f6c814e75e178038692838172
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-13 11:15:41 -0700

    example 1

--- OptionalClass/Rev.java
+++ OptionalClass/Rev.java
@@ -1,5 +1,14 @@
+import java.util.Optional;
+
 public class Rev {
     public static void main(String[] args) {
-        
+        String[] str = new String[5];
+        str[2] = "Revature classes are coming soon";
+
+        Optional<String> empty = Optional.empty();
+        System.out.println(empty);
+
+        Optional<String> value = Optional.of(str[2]);
+        System.out.println(value);
     }
 }
